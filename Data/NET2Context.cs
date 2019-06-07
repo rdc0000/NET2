fbdcc44021fdde36eb1f39fee217925b13fc09c9
rdc0000 <rdc.159@hotmail.com>
2019-06-07 13:12:01 -0500

    1

---
+++ Data/NET2Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NET2.Models;
+
+    public class NET2Context : DbContext
+    {
+        public NET2Context (DbContextOptions<NET2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NET2.Models.Categoria> Categoria { get; set; }
+    }
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace NET2
 {
@@ -33,6 +34,9 @@ namespace NET2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<NET2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("NET2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
