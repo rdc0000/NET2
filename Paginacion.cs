860b36f72ae16a165919c337c990c252a8c0a016
rdc0000 <rdc.159@hotmail.com>
2019-06-21 10:02:01 -0500

    lab3

--- Paginacion.cs
+++ Paginacion.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-namespace WebApp
+namespace Net2
 {
     public class Paginacion<T> : List<T>
     {
